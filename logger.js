e30ace0b5a1047b06e62fe4fa8cd8d75243ea4e6
Loshana Aloka <loshanaaloka27@gmail.com>
2022-09-03 19:04:29 +0530

    Fixed logger having a static time

--- logger.js
+++ logger.js
@@ -1,16 +1,19 @@
 // Just a quick way to log with timestamp (UTC)
 export default class logger_func {
   constructor() {
+    null;
+  }
+  get_time() {
     const d = new Date();
-    this.time = d.toUTCString();
+    return d.toUTCString();
   }
   async log(log_msg) {
-    console.log(`${this.time} >> ${log_msg}`);
+    console.log(`${this.get_time()} >> ${log_msg}`);
   }
   async warn(log_msg) {
-    console.log(`${this.time} >> ${log_msg}`);
+    console.log(`${this.get_time()} >> ${log_msg}`);
   }
   async error(log_msg) {
-    console.log(`${this.time} >> ${log_msg}`);
+    console.log(`${this.get_time()} >> ${log_msg}`);
   }
 }
