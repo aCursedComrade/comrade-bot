ea1cf494a8d611f459eb404d2f83334c585398c5
Loshana Aloka <acursed_comrade@proton.me>
2024-02-01 21:50:32 +0530

    Update `meme` command and minor changes

--- Discord/commands/meme.js
+++ Discord/commands/meme.js
@@ -1,22 +1,29 @@
 import { SlashCommandBuilder } from 'discord.js';
-import { get_reddit } from '../../functions/reddit.js';
 import { EmbedBuilder } from 'discord.js';
+import axios from 'axios';
 
-export const data = new SlashCommandBuilder().setName('meme').setDescription('Hot meme from r/dankmemes');
+export const data = new SlashCommandBuilder().setName('meme').setDescription('Hot meme from the internet');
+
 /**
  * @param {import('discord.js').ChatInputCommandInteraction} interaction
  */
 export async function handler(interaction) {
     await interaction.deferReply();
-    const meme = await get_reddit();
-    if (meme) {
-        const memepayload = new EmbedBuilder()
-            .setTitle(meme.title)
-            .setURL(`https://www.reddit.com${meme.permalink}`)
-            .setImage(meme.url)
-            .setFooter({ text: meme.subreddit_name_prefixed });
-        await interaction.followUp({ embeds: [memepayload.data] });
-    } else {
-        await interaction.editReply('Issue reaching to API. Try again later.');
-    }
+    await axios
+        .get('https://meme-api.com/gimme', {
+            headers: { 'User-Agent': 'Comrade-Bot https://github.com/aCursedComrade/Comrade-Bot' },
+        })
+        .then(async (res) => {
+            const message = new EmbedBuilder()
+                .setTitle(res.data.title)
+                .setURL(res.data.postLink)
+                .setImage(res.data.url)
+                .setFooter({ text: `r/${res.data.subreddit}` });
+
+            await interaction.followUp({ embeds: [message.toJSON()] });
+        })
+        .catch(async (err) => {
+            console.error(err.message);
+            await interaction.editReply('Issue reaching to API. Try again later.');
+        });
 }
